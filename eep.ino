d6818e0531412ead8b3bb0a875ec9da9f07e8f49
KH RACING <j_hirama2@yahoo.co.jp>
2024-07-15 21:27:23 +0900

    src commit

---
+++ eep.ino
@@ -0,0 +1,38 @@
+#include <EEPROM.h>
+/*
+   起動時のEEPデータ読み込み
+*/
+#define EEP_PASS "daiki111"
+void eep_data_get() {
+  EEPData eep;
+  // passworkcheck
+  EEPROM.get( 0, eep );
+  if ( memcmp(eep.password, EEP_PASS, 8) != 0) {
+    // パスワードNGなら初期値書き込み
+    memcpy(eep.password, EEP_PASS, 8);
+    eep.calc_v = 0.0048191593352884 +(0.0000012 * 100);
+    eep.dischg_v = 2.7;
+    eep.f_normal_target = 1.8;
+    eep.target_race_current = 2.0;
+    eep.target_normal_current = 3.0;
+    EEPROM.put( 0, eep );
+  }
+  CalcV = eep.calc_v;
+  f_volt[1] = eep.dischg_v;
+  f_normal_target = eep.f_normal_target;
+  target_race_current = eep.target_race_current;
+  target_normal_current = eep.target_normal_current;
+}
+/*
+   データ設定時ののEEPデータ書き込み
+*/
+void eep_data_put() {
+  EEPData eep;
+  memcpy(eep.password, EEP_PASS, 8);
+  eep.calc_v = CalcV;
+  eep.dischg_v = f_volt[1];
+  eep.f_normal_target = f_normal_target;
+  eep.target_race_current = target_race_current;
+  eep.target_normal_current = target_normal_current;
+  EEPROM.put( 0, eep );
+}
---
+++ eepdata.h
@@ -0,0 +1,8 @@
+typedef struct EEP_Data {
+  float calc_v;
+  float dischg_v;
+  float f_normal_target;
+  float target_race_current;
+  float target_normal_current;
+  char  password[8]; /* daiki1111*/
+} EEPData, *pEEPData;
